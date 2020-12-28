0ffbf0ec351dc20c6a168b812b1c150fcb045eef
Bob Casazza <rcasazza@gmail.com>
2020-12-27 19:09:52 -0500

    add immutability plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
         ecmaVersion: 12,
         sourceType: 'module',
     },
+    plugins: [
+      'immutable',
+    ],
     rules: {
         indent: ['error', 4],
+        'immutable/no-mutation': 'error',
     },
 };
